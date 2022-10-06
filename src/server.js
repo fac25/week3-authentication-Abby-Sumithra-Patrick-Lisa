e4de549340e00ac04e41f8df4e081f5fcd7f2d04
patdel0 <zefiru@protonmail.com>
2022-10-06 11:07:28 +0100

    Add support for `.env` file
    Co-authored-by: Abby <hello@abbychien.com>

--- src/server.js
+++ src/server.js
@@ -1,32 +1,35 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const home = require("./routes/home.js");
-const signup = require("./routes/sign-up.js");
-const login = require("./routes/log-in.js");
-const logout = require("./routes/log-out.js");
-const userpage = require("./routes/user-page.js");
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const home = require("./routes/home.js")
+const signup = require("./routes/sign-up.js")
+const login = require("./routes/log-in.js")
+const logout = require("./routes/log-out.js")
+const userpage = require("./routes/user-page.js")
 
-const bodyParser = express.urlencoded({ extended: false });
-const server = express();
+// Handle hidden variables
+const dotenv = require("dotenv")
+dotenv.config()
+
+const bodyParser = express.urlencoded({ extended: false })
+const server = express()
 
 //fix before deployment
-const cookies = cookieParser(process.env.COOKIE_SECRET || "secret");
+const cookies = cookieParser(process.env.COOKIE_SECRET || "secret")
 
-server.use(cookies);
-server.get("/", home.get);
 
-server.get("/sign-up", signup.get);
-server.post("/sign-up", bodyParser, signup.post);
-server.get("/log-in", login.get);
-server.post("/log-in", bodyParser, login.post);
-server.post("/log-out", logout.post);
-server.get("/user-page/:user_id", userpage.get);
-server.post("/user-page/:user_id", bodyParser, userpage.post);
+server.use(cookies)
+server.get("/", home.get)
 
+server.get("/sign-up", signup.get)
+server.post("/sign-up", bodyParser, signup.post)
+server.get("/log-in", login.get)
+server.post("/log-in", bodyParser, login.post)
+server.post("/log-out", logout.post)
+server.get("/user-page/:user_id", userpage.get)
+server.post("/user-page/:user_id", bodyParser, userpage.post)
 
 function sanitize(str) {
-    return str.replaceAll('<', '&lt;')
+  return str.replaceAll("<", "&lt;")
 }
 
-
-module.exports = server;
+module.exports = server
