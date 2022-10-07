1c02ccdf0fea9cdd84b5215c19848c95c28a0112
Abbbyccc <abbychien@hotmail.com>
2022-10-07 10:22:42 +0100

    connect .env

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 const { readFileSync } = require("node:fs");
 const { join } = require("node:path");
 const Database = require("better-sqlite3");
-
+require('dotenv').config()
 const db = new Database(process.env.DB_FILE);
 
 const schemaPath = join("src", "database", "schema.sql");
