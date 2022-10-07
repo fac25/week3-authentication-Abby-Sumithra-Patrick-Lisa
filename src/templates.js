12994d7bc406b8737404994f03b4004cbe702dd3
patdel0 <zefiru@protonmail.com>
2022-10-07 11:28:20 +0100

    Add Accessibility
    Co-authored-by: Sumithra <90449646+Sumithra-Suresh@users.noreply.github.com
    >

--- src/templates.js
+++ src/templates.js
@@ -1,5 +1,4 @@
-const { deleteBooks } = require("./model/books.js");
-
+const { deleteBooks } = require("./model/books.js")
 
 function Layout({ title, content }) {
   return /*html*/ `
@@ -17,11 +16,11 @@ function Layout({ title, content }) {
           </div>
         </body>
       </html>
-    `;
+    `
 }
 
 function Table({ data }) {
-  const [, name, author, rating] = Object.keys(data[0]);
+  const [, name, author, rating] = Object.keys(data[0])
   return /*html*/ `
   <div>
   <table>
@@ -38,7 +37,7 @@ function Table({ data }) {
 
   </table>
   </div>
-  `;
+  `
 }
 
 function Row({ id, name, author, rating }) {
@@ -47,15 +46,16 @@ function Row({ id, name, author, rating }) {
         <td>${name}</td>
         <td>${author}</td>
         <td>${rating}</td>
-        <td><form method='POST' action='/delete-book/${id}'><button>deleteBooks</button></form></td>
-    </tr>
-  `;
+        <td><form  method='POST' action='/delete-book/${id}'>
+        <button class='delete-btn'>Delete</button></form></td>
+    </tr> 
+  `
 }
 
 function getUserPage({ session, books, errors }) {
   let content = /*html*/ `
   <nav>
-  ${session ? `<a href='/'>Recommended Books</a>`: ''}
+  ${session ? `<a href='/'>Recommended Books</a>` : ""}
   ${displayLogout(session)}
   </nav>
     <br/>
@@ -63,37 +63,43 @@ function getUserPage({ session, books, errors }) {
       <form method="POST" class='books-form' > 
       <label for="book">Book name</label>
       <input id="book" name="book">
-      ${errors ? `<h3 style="color:red"  aria-describedby="book">Please enter your favorite book</h3>` : ''}
+      ${
+        errors
+          ? `<h3 style="color:red"  aria-describedby="book">Please enter your favorite book</h3>`
+          : ""
+      }
       <label for="author">Author</label>
       <input id="author" name="author">
-      ${errors ? `<h3 style="color:red" aria-describedby="author">Please enter the name of the author</h3>` : ''}
+      ${
+        errors
+          ? `<h3 style="color:red" aria-describedby="author">Please enter the name of the author</h3>`
+          : ""
+      }
       <label for="rating">Rating</label>
       <input id="rating" type="range" name="rating" min="0" max="5" required>
       <label for="sharing">Recommend to others</label>
       <input id="sharing" type="checkbox" name="sharing">
       <button >Submit</button>
       </form>
-      ${books.length && Table({caption:"User books", data:books})}
+      ${books.length && Table({ caption: "User books", data: books })}
     </div>
   `
-  
-  return Layout({title: 'My books', content})
 
+  return Layout({ title: "My books", content })
 }
-
 
 function displayLogout(session) {
-  return `${session ?
-    `<form method="POST" action="/log-out"><button>Log out</button></form>` :
-    `<div class='register-btns'><a class='signup' href="/sign-up">Sign up</a>  <a class='login' href="/log-in">Log In</a></div>`}`
+  return `${
+    session
+      ? `<form method="POST" action="/log-out"><button>Log out</button></form>`
+      : `<div class='register-btns'><a class='signup' href="/sign-up">Sign up</a>  <a class='login' href="/log-in">Log In</a></div>`
+  }`
 }
 
-
-
 function Home({ session, sharedBooks }) {
-  const content = /*html*/` 
+  const content = /*html*/ ` 
   <nav>
-    ${session ? `<a href='/user-page/${session?.user_id}'>My Books</a>`: ''}
+    ${session ? `<a href='/user-page/${session?.user_id}'>My Books</a>` : ""}
     ${displayLogout(session)}
   </nav>
   <div>
@@ -105,19 +111,21 @@ function Home({ session, sharedBooks }) {
       </ul>
     </div>`
 
-  return Layout({ title: 'WEBSITE NAME', content })
+  return Layout({ title: "WEBSITE NAME", content })
 }
 
 function bookList(arr) {
-  return arr.map(book => `
+  return arr
+    .map(
+      (book) => `
   <li>
     <p>${book.name}</p>
     <p>${book.author}</p>
     <p>Rating: ${book.rating}</p> 
   </li>`
-  ).join("")
+    )
+    .join("")
 }
-
 
 function SignUp(error) {
   const content = `
@@ -127,17 +135,25 @@ function SignUp(error) {
       <div>
         <label for="email">Email</label>
         <input type="email" id="email" name="email" >
-        ${error ? `<h3 style="color:red" aria-describedby="email">Please enter your email</h3>` : ''}
+        ${
+          error
+            ? `<h3 style="color:red" aria-describedby="email">Please enter your email</h3>`
+            : ""
+        }
       </div>
       <div>
         <label for="password">Password</label>
         <input type="password" id="password" name="password" >
-        ${error ? `<h3 style="color:red" aria-describedby="email">Please enter your password</h3>` : ''}
+        ${
+          error
+            ? `<h3 style="color:red" aria-describedby="email">Please enter your password</h3>`
+            : ""
+        }
       </div>
       <button>Sign up</button>
     </form>
   </div>`
-  return Layout({ title: 'Sign Up', content })
+  return Layout({ title: "Sign Up", content })
 }
 
 function Login(error) {
@@ -148,18 +164,26 @@ function Login(error) {
         <div>
           <label for="email">Email</label>
           <input type="email" id="email" name="email" >
-          ${error ? `<h3 style="color:red" aria-describedby="email">Please enter your email</h3>` : ''}
+          ${
+            error
+              ? `<h3 style="color:red" aria-describedby="email">Please enter your email</h3>`
+              : ""
+          }
         </div>
         <div>
           <label for="password">Password</label>
           <input type="password" id="password" name="password">
-          ${error ? `<h3 style="color:red" aria-describedby="email">Please enter your password</h3>` : ''}
+          ${
+            error
+              ? `<h3 style="color:red" aria-describedby="email">Please enter your password</h3>`
+              : ""
+          }
         </div>
         <button>Log in</button>
       </form>
     </div>
-  `;
-  return Layout({ title: 'Log In', content })
+  `
+  return Layout({ title: "Log In", content })
 }
 
 module.exports = { Layout, Home, SignUp, Login, getUserPage }
